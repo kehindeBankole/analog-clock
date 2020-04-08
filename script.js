d39724a28d5e20f604c084a1ac3d18ef11874cb3
kehinde <bankolek1@gmail.com>
2020-04-09 00:07:03 +0100

    added few things

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ let sec = document.getElementById("sec")
 let min = document.getElementById("min")
 let hr = document.getElementById("hr")
 let date = new Date()
-
 function changesec(){
    sec.style.transform=`rotate(${(new Date().getUTCSeconds() / 60) * 360}deg)`
    min.style.transform=`rotate(${(new Date().getUTCMinutes() / 60) * 360}deg)`
    hr.style.transform=`rotate(${(new Date().getHours() / 12) * 360}deg)`
 }
-
 setInterval(()=>{
     changesec();
 },1000)
